29b375d6a1664b7f0920bf10426c60283ccef085
raphael letendu <raphael.letendu@microchip.com>
2020-09-16 09:59:19 +0200

    Changed BMG extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -742,7 +742,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 415 } // E0 93 -> 415 for BMG
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1095,7 +1095,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false // true on default Ender extruder, false with BMG
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
